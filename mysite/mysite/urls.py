c39c0a4a779e72f7ba93b7ecf95b63bc7f6a1bd0
Albert Ribes <albert.ribes@gmail.com>
2017-05-10 12:03:31 +0200

    New update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from . import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'^polls/', include('polls.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+# Create your views here.
+from django.shortcuts import redirect
+from django.utils import timezone
 
-def index(request):
-    html= """<b>My personal Site</b> <br/>
-          <i>author: Albert Ribes</i><br/>
-	  <hr>
-          <a href="/polls/">Polls</a> <br/>
-          <a href="/blog/">Blog</a> <br/>
-          <a href="/admin/">Admin</a> <br/>
-	  <a href="/contact/">Contact</a>"""
-    return HttpResponse(html)
+def home(request):
+    return render(request, 'mysite/home.html',)
+
