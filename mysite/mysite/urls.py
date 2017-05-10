03312d88e68cd5c8755ec0ebc7f143ec076943a9
Albert Ribes <albert.ribes@gmail.com>
2017-05-10 17:28:22 +0200

    New Update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 from django.contrib.auth.views import login, logout
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
@@ -29,3 +31,6 @@ urlpatterns = [
     url(r'^contact/', include('contact.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
